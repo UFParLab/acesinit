ef793e4458b31cbf3fe375732b6fdf97d19b0baa
Nakul Jindal <nakul02@ufl.edu>
2013-10-23 16:12:31 -0400

    Added option to specify output filename

--- src/aces_setup/setup_main_entry.c
+++ src/aces_setup/setup_main_entry.c
@@ -1,14 +1,55 @@
 #include <f77_name.h>
 #include <f_types.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+// Based on the example provided at 
+// http://www.ibm.com/developerworks/aix/library/au-unix-getopt.html
+
+static const char *optString = "o:h?";
 
 #ifdef FC_MAIN
-int FC_MAIN(int argc, const char * const* argv) 
+int FC_MAIN(int argc, char* const* argv) 
 #elif defined(F77_MAIN)
-int F77_MAIN(int argc, const char * const* argv)
+int F77_MAIN(int argc, char* const * argv)
 #else
-int main (int argc, const char* const* argv)
+int main (int argc, char* const* argv)
 #endif
 {
-       F77_NAME(setup_main, SETUP_MAIN)();
+    int opt = 0;
+    char *fName = NULL;
+    char *defaultName = "data";
+
+    opt = getopt(argc, argv, optString);
+    while ( opt != -1) {
+        switch (opt) {
+            case 'o':
+                fName = optarg;
+                //printf ("DEBUG : %s\n", optarg);
+                break;
+            case 'h' : case '?':
+            default :
+                fprintf (stderr, "Usage : %s -o <output_name_without_.dat>\n", argv[0]);
+                return -1;
+                break;
+        }
+
+        opt = getopt ( argc, argv, optString ) ;
+
+    }
+
+    if (fName == NULL){
+        fName = defaultName;
+    }
+
+    int len = strlen(fName);
+    char *datName = (char*)malloc(len + 4);   // "fName.dat + \0"
+    sprintf(datName, "%s.dat", fName);
+
+    printf ("Output is set as %s\n", fName);
+    
+    F77_NAME(setup_main, SETUP_MAIN)(fName, datName);
 }
 
