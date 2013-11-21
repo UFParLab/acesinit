9d5df68078aedd2666a3c50cbf1ba7405d642b97
Victor Lotrich <lotrich@qtp.ufl.edu>
2013-11-21 16:20:36 -0500

    works correctly as init for integral calculation

--- src/include/basis_info.h
+++ src/include/basis_info.h
@@ -1,13 +1,15 @@
-      common/basis_info/ntot_alpha, ntot_pcoeff, ntot_shells,  
-     *                  Talpha, Tpcoeff, Tixalpha, Tixpcoeff 
+      common/basis_info/ntot_alpha, ntot_pcoef, ntot_shells,  
+     *                  Talpha, Tpcoeff, Tixalpha, Tixpcoeff, 
+     *                  Tcoord 
 
-      integer ntot_alpha, ntot_pcoeff, ntot_shells 
-      integer maxcoeff, maxalpha, maxshells   
-      parameter (maxcoeff = 100000)
-      parameter (maxalpha = 100000)
-      parameter (maxshells = 100000)
-      integer Talpha(maxalpha)
-      integer Tpcoeff(maxcoeff)
+      integer ntot_alpha, ntot_pcoef, ntot_shells 
+      integer maxcoeff, maxalpha, maxshells    
+      parameter (maxcoeff = 10000)
+      parameter (maxalpha = 10000)
+      parameter (maxshells = 10000)
+      double precision Talpha(maxalpha)
+      double precision Tpcoeff(maxcoeff)
+      double precision Tcoord(3,maxshells)
       integer Tixalpha(maxshells)
       integer Tixpcoeff(maxshells)
 
