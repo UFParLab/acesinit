da6e8593f0d81a2f678d66dfbf91e8923de5f821
njindal <njindal@cise.ufl.edu>
2014-10-22 01:17:58 -0400

    Added CMake Build system

--- src/include/f77_name.h
+++ src/include/f77_name.h
@@ -2,32 +2,44 @@
 #define _F77_NAME_H_
 
 #ifdef HAVE_CONFIG_H
+// From Autoconf
+
 #include <config.h>
-#endif
+
 
 #define F77_NAME(name, NAME) F77_FUNC(name, NAME)
 #define F77_CB_NAME(name, NAME) F77_FUNC(name, NAME)
 
-#ifdef OLD_MANGLING
-#ifdef C_UPPER
-#      define F77_NAME(name,NAME) NAME
-#else
-#   ifdef C_SUFFIX
-#      define F77_NAME(name,NAME) name##_
-#   else
-#      define F77_NAME(name,NAME) name
-#   endif /* C_SUFFIX */
-#endif /* C_UPPER */
+#else  // HAVE_CONFIG_H
 
-#ifdef CB_UPPER
-#      define F77_CB_NAME(name,NAME) NAME
-#else
-#   ifdef CB_SUFFIX
-#      define F77_CB_NAME(name,NAME) name##_
-#   else
-#      define F77_CB_NAME(name,NAME) name
-#   endif /* CB_SUFFIX */
-#endif /* CB_UPPER */
-#endif /*OLD_MANGLING */
+// From CMAKE
+#include "fc.h"
+#define F77_NAME(name, NAME) FC_GLOBAL(name, NAME)
+#define F77_CB_NAME(name, NAME) FC_GLOBAL(name, NAME)
+
+//#ifdef OLD_MANGLING
+//#ifdef C_UPPER
+//#      define F77_NAME(name,NAME) NAME
+//#else
+//#   ifdef C_SUFFIX
+//#      define F77_NAME(name,NAME) name##_
+//#   else
+//#      define F77_NAME(name,NAME) name
+//#   endif /* C_SUFFIX */
+//#endif /* C_UPPER */
+//
+//#ifdef CB_UPPER
+//#      define F77_CB_NAME(name,NAME) NAME
+//#else
+//#   ifdef CB_SUFFIX
+//#      define F77_CB_NAME(name,NAME) name##_
+//#   else
+//#      define F77_CB_NAME(name,NAME) name
+//#   endif /* CB_SUFFIX */
+//#endif /* CB_UPPER */
+//#endif /*OLD_MANGLING */
+//
+#endif // HAVE_CONFIG_H
+
 
 #endif /* _F77_NAME_H_ */
